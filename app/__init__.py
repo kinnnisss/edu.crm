898356eaf3f268a1874946e99dd8a0d6018a9842
kinnnisss <kinelame3@gmail.com>
2026-03-02 13:44:10 +0000

    teste de mon blue print Courses

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+# app/__init__.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "dev-secret-key"
+
+    # Blueprints
+    from app.courses import courses_bp
+    app.register_blueprint(courses_bp, url_prefix="/courses")
+
+    # from app.students import students_bp
+    # app.register_blueprint(students_bp, url_prefix="/students")
+    # from app.teachers import teachers_bp
+    # app.register_blueprint(teachers_bp, url_prefix="/teachers")
+
+#seulement pour tester mon blueprint courses
+    @app.route("/")
+    def home():
+        return "Edu.CRM OK ! (Va sur /courses)"
+
+    return app
